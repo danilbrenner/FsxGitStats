c33bb87f6191f4b4535efb3cecf2a82075b24b04
danilbrenner <danilbrenner@outlook.com>
2017-06-18 21:02:32 +0200

    Number of commits chart

--- src/App.fsx
+++ src/App.fsx
@@ -1,5 +1,28 @@
-
 #load "GitStatsReder.fsx"
+#load "CommitInfoModule.fsx"
+#load "../packages/FSharp.Charting/FSharp.Charting.fsx"
 
-GitStatsReder.readGitStats """Path"""
-|> Seq.iter (fun i -> printfn "%s" i)
+open FSharp.Charting
+
+let repoPath = """---"""
+
+let splitSeq fn sq =
+    let mutable inc = 0
+    sq
+    |> Seq.map (fun i ->
+        inc <- if fn(i) then inc+1 else inc
+        (inc, i)
+    )
+    |> Seq.groupBy fst
+    |> Seq.map (fun (_, ix) -> ix)
+
+let sq =
+    GitStatsReder.readGitStats repoPath
+    |> splitSeq (fun l -> l.StartsWith("commit "))
+    |> Seq.map CommitInfoModule.processCommitInfo
+
+sq
+|> Seq.countBy (fun i -> i.Date.Date)
+|> Seq.rev
+|> (fun i -> Chart.Line (i, Name = "Number of commits"))
+//---
